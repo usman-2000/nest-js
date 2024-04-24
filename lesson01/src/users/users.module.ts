d2b13d4618869a3d8cd1ccb2630c591635b06a66
usman-2000 <usmanrahim2000@gmail.com>
2024-04-24 17:54:37 +0500

    Controller file for user : nest g controller users

--- lesson01/src/users/users.module.ts
+++ lesson01/src/users/users.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
 
-@Module({})
-export class UsersModule {}
+@Module({
+    controllers: [UsersController],
+    providers: []
+})
+export class UsersModule {
+
+
+}
