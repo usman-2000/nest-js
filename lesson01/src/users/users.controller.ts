cb3b79ab0b1e4bc3fd2f28821714b9533faf5f45
usman-2000 <usmanrahim2000@gmail.com>
2024-06-12 07:58:47 +0500

    API ROUTES

--- lesson01/src/users/users.controller.ts
+++ lesson01/src/users/users.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('users') //  /users
 export class UsersController {
+
+    // Order does matter here when defining routes, we cannot define any route that is getting all data after we defined the route that is getting a single data using 'id'
     @Get() // /users
     findAll() {
         return []
     }
 
     @Get(":id") // /users/:id
-    findOne(id) {
+    findOne(@Param('id') id: string) {
+        return { id }
+    }
 
+    @Post() // Post /user
+    create(@Body() user: {}) {
+        return user
+    }
+
+    @Patch(":id") // /users/:id
+    update(@Param('id') id: string, @Body() userUpdate: {}) {
+        return { id, ...userUpdate }
     }
 }
