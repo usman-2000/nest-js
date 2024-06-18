ef09ab340067a38ea30e9208463ddbe0e69f34f4
usman-2000 <usmanrahim2000@gmail.com>
2024-06-18 12:27:14 +0500

    ParseIntPipe

--- lesson01/src/users/users.controller.ts
+++ lesson01/src/users/users.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
+
+// ParseIntPipe is a middleware to validate data that the given data is in numbers.
 
 @Controller('users') //  /users
 export class UsersController {
@@ -14,8 +16,8 @@ export class UsersController {
     }
 
     @Get(":id") // /users/:id
-    findOne(@Param('id') id: string) {
-        return this.usersService.findOne(+id)
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.findOne(id)
     }
 
     @Post() // Post /user
@@ -26,12 +28,12 @@ export class UsersController {
     }
 
     @Patch(":id") // PATCH  /users/:id
-    update(@Param('id') id: string, @Body() userUpdate: { name?: string, email?: string, role?: "Admin" | "Engineer" | "Intern" }) {
-        return this.usersService.update(+id, userUpdate)
+    update(@Param('id', ParseIntPipe) id: number, @Body() userUpdate: { name?: string, email?: string, role?: "Admin" | "Engineer" | "Intern" }) {
+        return this.usersService.update(id, userUpdate)
     }
 
     @Delete(":id") // DELETE  /users/:id
-    delete(@Param('id') id: string) {
-        return this.usersService.delete(+id)
+    delete(@Param('id', ParseIntPipe) id: number) {
+        return this.usersService.delete(id)
     }
 }
