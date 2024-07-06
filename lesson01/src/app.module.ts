66cb7e574dddcc3a6faa72ee33f873330c52bdcd
usman-2000 <usmanrahim2000@gmail.com>
2024-07-06 17:25:29 +0500

    created resource employee and database

--- lesson01/src/app.module.ts
+++ lesson01/src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { LeadersModule } from './leaders/leaders.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule, LeadersModule],
+  imports: [UsersModule, LeadersModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
